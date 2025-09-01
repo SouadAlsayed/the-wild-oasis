99f28bde9cfc31f0862353c0777cc9f4ccaa62e1
SouadAlsayed <souadalsayed1804@gmail.com>
2025-09-01 17:09:13 +0300

    feat(layout): implement AppLayout with Sidebar and Header components

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,30 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import styled from "styled-components";
+
+const StyledAppLayout = styled.div`
+  background-color: var(--color-grey-50);
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+function AppLayout() {
+  return (
+    <StyledAppLayout>
+      <Sidebar />
+      <Header />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
+  );
+}
+
+export default AppLayout;
