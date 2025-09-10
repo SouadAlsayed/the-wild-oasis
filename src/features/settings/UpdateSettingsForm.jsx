d3d5a5e918d87fba593a359f5d97e36826276bd9
SouadAlsayed <souadalsayed1804@gmail.com>
2025-09-10 18:20:26 +0300

    feat(settings): implement UpdateSettingsForm and associated hooks for managing hotel settings

---
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -0,0 +1,71 @@
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import Spinner from "../../ui/Spinner";
+import { useSettings } from "./useSettings";
+import { useUpdateSetting } from "./useUpdateSetting";
+
+function UpdateSettingsForm() {
+  const {
+    isLoading,
+    settings: {
+      minBookingLength,
+      maxBookingLength,
+      maxGuestsPerBooking,
+      breakfastPrice,
+    } = {},
+  } = useSettings();
+
+  const { isUpdating, updateSetting } = useUpdateSetting();
+
+  function handleUpdate(e, field) {
+    const { value } = e.target;
+
+    if (!value) return;
+    updateSetting({ [field]: value });
+  }
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <Form>
+      <FormRow label="Minimum nights/booking">
+        <Input
+          onBlur={(e) => handleUpdate(e, "minBookingLength")}
+          type="number"
+          id="min-nights"
+          defaultValue={minBookingLength}
+        />
+      </FormRow>
+
+      <FormRow label="Maximum nights/booking">
+        <Input
+          onBlur={(e) => handleUpdate(e, "maxBookingLength")}
+          type="number"
+          id="max-nights"
+          defaultValue={maxBookingLength}
+        />
+      </FormRow>
+
+      <FormRow label="Maximum guests/booking">
+        <Input
+          onBlur={(e) => handleUpdate(e, "maxGuestsPerBooking")}
+          type="number"
+          id="max-guests"
+          defaultValue={maxGuestsPerBooking}
+        />
+      </FormRow>
+
+      <FormRow label="Breakfast price">
+        <Input
+          onBlur={(e) => handleUpdate(e, "breakfastPrice")}
+          type="number"
+          id="breakfast-price"
+          defaultValue={breakfastPrice}
+        />
+      </FormRow>
+    </Form>
+  );
+}
+
+export default UpdateSettingsForm;
