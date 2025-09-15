7fa16d3b5955cf16b6248791e3246b1cf57b283a
SouadAlsayed <souadalsayed1804@gmail.com>
2025-09-15 19:51:57 +0300

    feat(bookings): add filtering and sorting to booking table

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,30 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // : { field: "totalPrice", value: 5000, method: "gte" };
+
+  // SORT
+  const sortByRow = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRow.split("-");
+  const sortBy = { field, direction };
+
   const {
     data: bookings,
     error,
     isLoading,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return {
     bookings,
